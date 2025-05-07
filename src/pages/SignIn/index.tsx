1c0a16fe41d8be2d35393d977c780243430cdb9f
elshaddai tambuwun <elshatambuwun17@gmail.com>
2025-05-07 10:45:02 +0700

    Firebase SignIn and SignUp Done

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,32 @@
 import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {Finote} from '../../assets';
 import Gap from '../../components/atoms/Gap';
 import Button from '../../components/atoms/Button';
 import TextInput from '../../components/moleculs/TextInput';
 import {Google} from '../../assets';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { showMessage } from 'react-native-flash-message';
 
 const LoginPage = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password).then(userCredential => {
+      const user = userCredential.user;
+      navigation.navigate('DashboardPage', {uid: user.uid});
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      showMessage({
+        message: errorMessage,
+        type: 'danger',
+      });
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Image source={Finote} style={styles.logo} resizeMode="contain" />
@@ -19,16 +39,16 @@ const LoginPage = ({navigation}) => {
       </TouchableOpacity>
       <Gap height={20} />
       <View style={styles.inputWrapper}>
-        <TextInput placeholder="Your Email" keyboardType="email-address" />
+        <TextInput placeholder="Your Email" keyboardType="email-address" value={email} onChangeText={value=>setEmail(value)} />
         <Gap height={20} />
 
-        <TextInput placeholder="Password" secureTextEntry />
+        <TextInput placeholder="Password" secureTextEntry value={password} onChangeText={value => setPassword(value)} />
       </View>
       <Gap height={30} />
 
       <Button
         label="Login"
-        onPress={() => navigation.navigate('BottomTabs')}
+        onPress={onSubmit}
         height={55}
       />
     </View>
